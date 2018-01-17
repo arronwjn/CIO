0a220e7908eeedac3009d3e072a51ee1cc094e5e
wangjiannan <904089747@qq.com>
2018-01-17 10:38:41 +0800

    add mUtils

--- src/config/mUtils.js
+++ src/config/mUtils.js
@@ -2,29 +2,87 @@
  * 存储localStorage
  */
 export const setStore = (name, content) => {
-	if (!name) return;
-	if (typeof content !== 'string') {
-		content = JSON.stringify(content);
-	}
-	window.localStorage.setItem(name, content);
+    if (!name) return;
+    if (typeof content !== 'string') {
+        content = JSON.stringify(content);
+    }
+    window.localStorage.setItem(name, content);
 }
 
 /**
  * 获取localStorage
  */
 export const getStore = name => {
-	if (!name) return;
-	return window.localStorage.getItem(name);
+    if (!name) return;
+    return window.localStorage.getItem(name);
 }
 
 /**
  * 删除localStorage
  */
 export const removeStore = name => {
-	if (!name) return;
-	window.localStorage.removeItem(name);
+    if (!name) return;
+    window.localStorage.removeItem(name);
 }
+/**
+ * 获取时间戳
+ */
+export const timeDiff = stringTime => {
+    var timestamp2 = Date.parse(new Date(stringTime));
+    timestamp2 = timestamp2 / 1000;
+    return timestamp2
+}
+/**
+ * 验证手机号格式
+ */
+export const isMobil = function (s) {
+    var patrn = /^1(3|4|5|7|8)\d{9}$/;
+    if (!patrn.exec(s)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+/**
+ * 验证出生日期
+ */
+export const isBirthday = function (data) {
+    var filter = /^\d{4}\-\d{2}\-\d{2}$/
 
+    if (filter.test(data)) {
+        return true
+    } else {
+        return false
+    }
+}
+/**
+ * 去除空格
+ */
+export const trim = function (str) {
+    return str.replace(/(^\s+)|(\s+$)/g, "");
+}
+/**
+ * 验证邮箱
+ */
+export const isEmail = function (mail) {
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (filter.test(mail)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/**
+ * 验证身份证
+ */
+export const isCard = function (card) {
+    var patrn = /^\d{15}|\d{}18$/;
+    if (!patrn.exec(card)) {
+        return false;
+    } else {
+        return true;
+    }
+}
 /**
  * 获取style样式
  */
@@ -33,48 +91,48 @@ export const getStyle = (element, attr, NumberMode = 'int') => {
     // scrollTop 获取方式不同，没有它不属于style，而且只有document.body才能用
     if (attr === 'scrollTop') {
         target = element.scrollTop;
-    }else if(element.currentStyle){
+    } else if (element.currentStyle) {
         target = element.currentStyle[attr];
-    }else{
-        target = document.defaultView.getComputedStyle(element,null)[attr];
+    } else {
+        target = document.defaultView.getComputedStyle(element, null)[attr];
     }
     //在获取 opactiy 时需要获取小数 parseFloat
-    return  NumberMode == 'float'? parseFloat(target) : parseInt(target);
+    return NumberMode == 'float' ? parseFloat(target) : parseInt(target);
 }
 
 /**
  * 页面到达底部，加载更多
  */
 export const loadMore = (element, callback) => {
-	let windowHeight = window.screen.height;
-	let height;
-	let setTop;
-	let paddingBottom;
-	let marginBottom;
+    let windowHeight = window.screen.height;
+    let height;
+    let setTop;
+    let paddingBottom;
+    let marginBottom;
     let requestFram;
     let oldScrollTop;
 
-    document.body.addEventListener('scroll',() => {
-       loadMore();
+    document.body.addEventListener('scroll', () => {
+        loadMore();
     }, false)
     //运动开始时获取元素 高度 和 offseTop, pading, margin
-	element.addEventListener('touchstart',() => {
+    element.addEventListener('touchstart', () => {
         height = element.offsetHeight;
         setTop = element.offsetTop;
-        paddingBottom = getStyle(element,'paddingBottom');
-        marginBottom = getStyle(element,'marginBottom');
-    },{passive: true})
+        paddingBottom = getStyle(element, 'paddingBottom');
+        marginBottom = getStyle(element, 'marginBottom');
+    }, { passive: true })
 
     //运动过程中保持监听 scrollTop 的值判断是否到达底部
-    element.addEventListener('touchmove',() => {
-       loadMore();
-    },{passive: true})
+    element.addEventListener('touchmove', () => {
+        loadMore();
+    }, { passive: true })
 
     //运动结束时判断是否有惯性运动，惯性运动结束判断是非到达底部
-    element.addEventListener('touchend',() => {
-       	oldScrollTop = document.body.scrollTop;
-       	moveEnd();
-    },{passive: true})
+    element.addEventListener('touchend', () => {
+        oldScrollTop = document.body.scrollTop;
+        moveEnd();
+    }, { passive: true })
 
     const moveEnd = () => {
         requestFram = requestAnimationFrame(() => {
@@ -82,10 +140,10 @@ export const loadMore = (element, callback) => {
                 oldScrollTop = document.body.scrollTop;
                 loadMore();
                 moveEnd();
-            }else{
-            	cancelAnimationFrame(requestFram);
-            	//为了防止鼠标抬起时已经渲染好数据从而导致重获取数据，应该重新获取dom高度
-            	height = element.offsetHeight;
+            } else {
+                cancelAnimationFrame(requestFram);
+                //为了防止鼠标抬起时已经渲染好数据从而导致重获取数据，应该重新获取dom高度
+                height = element.offsetHeight;
                 loadMore();
             }
         })
@@ -105,28 +163,28 @@ export const showBack = callback => {
     let requestFram;
     let oldScrollTop;
 
-    document.addEventListener('scroll',() => {
-       showBackFun();
+    document.addEventListener('scroll', () => {
+        showBackFun();
     }, false)
-    document.addEventListener('touchstart',() => {
-       showBackFun();
-    },{passive: true})
+    document.addEventListener('touchstart', () => {
+        showBackFun();
+    }, { passive: true })
 
-    document.addEventListener('touchmove',() => {
-       showBackFun();
-    },{passive: true})
+    document.addEventListener('touchmove', () => {
+        showBackFun();
+    }, { passive: true })
 
-    document.addEventListener('touchend',() => {
+    document.addEventListener('touchend', () => {
         oldScrollTop = document.body.scrollTop;
         moveEnd();
-    },{passive: true})
+    }, { passive: true })
 
     const moveEnd = () => {
         requestFram = requestAnimationFrame(() => {
             if (document.body.scrollTop != oldScrollTop) {
                 oldScrollTop = document.body.scrollTop;
                 moveEnd();
-            }else{
+            } else {
                 cancelAnimationFrame(requestFram);
             }
             showBackFun();
@@ -137,7 +195,7 @@ export const showBack = callback => {
     const showBackFun = () => {
         if (document.body.scrollTop > 500) {
             callback(true);
-        }else{
+        } else {
             callback(false);
         }
     }
@@ -159,7 +217,7 @@ export const animate = (element, target, duration = 400, mode = 'ease-out', call
     if (duration instanceof Function) {
         callback = duration;
         duration = 400;
-    }else if(duration instanceof String){
+    } else if (duration instanceof String) {
         mode = duration;
         duration = 400;
     }
@@ -188,7 +246,7 @@ export const animate = (element, target, duration = 400, mode = 'ease-out', call
     Object.keys(target).forEach(attr => {
         if (/[^\d^\.]+/gi.test(target[attr])) {
             unit[attr] = target[attr].match(/[^\d^\.]+/gi)[0] || 'px';
-        }else{
+        } else {
             unit[attr] = 'px';
         }
         initState[attr] = attrStyle(attr);
@@ -197,8 +255,8 @@ export const animate = (element, target, duration = 400, mode = 'ease-out', call
     //去掉传入的后缀单位
     Object.keys(target).forEach(attr => {
         if (unit[attr] == 'rem') {
-            target[attr] = Math.ceil(parseInt(target[attr])*rootSize);
-        }else{
+            target[attr] = Math.ceil(parseInt(target[attr]) * rootSize);
+        } else {
             target[attr] = parseInt(target[attr]);
         }
     });
@@ -213,30 +271,30 @@ export const animate = (element, target, duration = 400, mode = 'ease-out', call
             let iCurrent = attrStyle(attr) || 0; //当前元素属性址
             let speedBase = 0; //目标点需要减去的基础值，三种运动状态的值都不同
             let intervalTime; //将目标值分为多少步执行，数值越大，步长越小，运动时间越长
-            switch(mode){
+            switch (mode) {
                 case 'ease-out':
                     speedBase = iCurrent;
-                    intervalTime = duration*5/400;
+                    intervalTime = duration * 5 / 400;
                     break;
                 case 'linear':
                     speedBase = initState[attr];
-                    intervalTime = duration*20/400;
+                    intervalTime = duration * 20 / 400;
                     break;
                 case 'ease-in':
                     let oldspeed = remberSpeed[attr] || 0;
-                    iSpeed = oldspeed + (target[attr] - initState[attr])/duration;
+                    iSpeed = oldspeed + (target[attr] - initState[attr]) / duration;
                     remberSpeed[attr] = iSpeed
                     break;
                 default:
                     speedBase = iCurrent;
-                    intervalTime = duration*5/400;
+                    intervalTime = duration * 5 / 400;
             }
             if (mode !== 'ease-in') {
                 iSpeed = (target[attr] - speedBase) / intervalTime;
                 iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
             }
             //判断是否达步长之内的误差距离，如果到达说明到达目标点
-            switch(mode){
+            switch (mode) {
                 case 'ease-out':
                     status = iCurrent != target[attr];
                     break;
@@ -258,7 +316,7 @@ export const animate = (element, target, duration = 400, mode = 'ease-out', call
                     element.style.opacity = (iCurrent + iSpeed) / 100;
                 } else if (attr === 'scrollTop') {
                     element.scrollTop = iCurrent + iSpeed;
-                }else{
+                } else {
                     element.style[attr] = iCurrent + iSpeed + 'px';
                 }
             } else {
